5b512e20c0a07494e248723d0497c2a75ce9c389
RamuljAdnan <adnanramulj@gmail.com>
2018-03-31 17:27:14 +0200

    Added_navDrawer, test data input

--- app/src/main/java/com/readingtrackerapp/activities/MainActivity.java
+++ app/src/main/java/com/readingtrackerapp/activities/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +17,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
 
 import com.readingtrackerapp.R;
 import com.readingtrackerapp.adapters.TabAdapter;
@@ -27,22 +32,27 @@ import java.util.Calendar;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
     DBHandler dbHandler;
     Toolbar toolbar;
     ViewPager viewPager;
     TabLayout tabLayout;
+    TextView username;
+    NavigationView mNavigationView;
+    View mHeaderView;
+    String username_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         dbHandler = new DBHandler(getApplicationContext());
 
         // checks if user is registered
         isUserRegistered();
+
 
         // set toolbar and options menu for sorting books
         setToolbar();
@@ -50,11 +60,14 @@ public class MainActivity extends AppCompatActivity {
         // set view pager
         setViewPager();
 
+        //initialize nav drawer and put username in header
+        InitializeNavDrawer();
     }
 
     private void setAlarms() {
 
         MyAlarmManager myAlarmManager=new MyAlarmManager(getApplicationContext());
+
 
         Cursor c = dbHandler.getCurrentlyReadingBooks(true, DBContractClass.BOOK.COLUMN_TITLE, "");
         while (c.moveToNext())
@@ -64,6 +77,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void InitializeNavDrawer()
+    {
+
+        // NavigationView
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        // NavigationView Header
+        mHeaderView =  mNavigationView.getHeaderView(0);
+        username=(TextView) mHeaderView.findViewById(R.id.user_name);
+        username.setText(username_text);
+
+        drawerLayout=(DrawerLayout) findViewById(R.id.content);
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     // user registration
     private void isUserRegistered() {
@@ -75,6 +115,8 @@ public class MainActivity extends AppCompatActivity {
             Snackbar.make(findViewById(R.id.content), "Wellcome back " + user.getName().toUpperCase() + " " + user.getSurname().toUpperCase(), Snackbar.LENGTH_SHORT)
                     .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
                     .show();
+            username_text=user.getName().toUpperCase() + " " + user.getSurname().toUpperCase();
+
         } else
             startActivity(new Intent(MainActivity.this, RegisterUser.class));
 
@@ -87,8 +129,12 @@ public class MainActivity extends AppCompatActivity {
 
         // setting view pager with fragments and titles to show on tabs
         TabAdapter adapter = new TabAdapter(getSupportFragmentManager());
+
+
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        dbHandler.getCountOfRecords();
 
     }
 
